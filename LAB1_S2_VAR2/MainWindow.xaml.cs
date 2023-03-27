67cd12de53b6afe659e04314c1f1a0a9db4106d8
jur_chik <jur_chik@DESKTOP-5OBRJHP>
2023-03-27 21:46:33 +0300

    Добавьте файлы проекта.

---
+++ LAB1_S2_VAR2/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CLASS_LIB;
+
+namespace LAB1_S2_VAR2 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        ViewData viewData = new ViewData();
+        public MainWindow() {
+            InitializeComponent();
+            this.DataContext = viewData;
+
+            init_type.ItemsSource = Enum.GetValues(typeof(FRawEnum));
+        }
+
+        private void create_Click(object sender, RoutedEventArgs e) {
+            try {
+                viewData.ExiquteSpline();
+                integral.Text = viewData.Spline.IntergralVal.ToString();
+                string[] info = new string[viewData.NodesNum];
+                for (int i = 0; i < viewData.NodesNum; i++) {
+                    info[i] = $"Coord = {viewData.Raw.Coord[i].ToString("0.000")}\nVal = {viewData.Raw.Val[i].ToString("0.000")}";
+                }
+                RawData_info.ItemsSource = info;
+            }
+            catch (Exception ex){
+                string messageBoxText = ex.Message;
+                string caption = "Ошибочка";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBoxResult result;
+                result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e) {
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.FileName = "rawdata";
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text documents (.txt)|*.txt";
+            dialog.ShowDialog();
+
+            viewData.CreateRawData();
+            viewData.Raw.Save(dialog.FileName);
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e) {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "rawdata";
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text documents (.txt)|*.txt";
+            dialog.ShowDialog();
+
+            try {
+                viewData.Load(dialog.FileName);
+                viewData.ExiquteSpline();
+                integral.Text = viewData.Spline.IntergralVal.ToString();
+
+                string[] info = new string[viewData.NodesNum];
+                for (int i = 0; i < viewData.NodesNum; i++) {
+                    info[i] = $"Coord = {viewData.Raw.Coord[i].ToString("0.000")}\nVal = {viewData.Raw.Val[i].ToString("0.000")}";
+                }
+                RawData_info.ItemsSource = info;
+            }
+            catch (Exception ex) {
+                string messageBoxText = ex.Message;
+                string caption = "Ошибочка";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBoxResult result;
+                result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+            }
+        }
+    }
+}
---
+++ LAB1_S2_VAR2/ViewData.cs
@@ -0,0 +1,127 @@
+﻿using CLASS_LIB;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Documents;
+
+namespace LAB1_S2_VAR2 {
+    public class ViewData : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        //для RawData
+        private int nodesnum;
+        public int NodesNum { 
+            get { return nodesnum;  }
+            set {
+                nodesnum = value;
+                OnPropertyChanged("NodesNum");
+            } 
+        }
+        private double lend;
+        public double LEnd { 
+            get { return lend;  } 
+            set {
+                lend = value;
+                OnPropertyChanged("LEnd");
+            } 
+        }
+        private double rend;
+        public double REnd {
+            get { return rend; }
+            set {
+                rend = value;
+                OnPropertyChanged("REnd");
+            }
+        }
+
+        private bool isuniform;
+        public bool IsUnuform {
+            get { return isuniform; }
+            set {
+                isuniform = value;
+                OnPropertyChanged("IsUnuform");
+            } 
+        }
+        private FRawEnum functype;
+        public FRawEnum FuncType {
+            get { return functype; }
+            set {
+                functype = value;
+                OnPropertyChanged("FuncType");
+            }
+        }
+        public RawData? Raw { get; set; }
+
+        //для SplineData
+        public int SplineNodesNum { get; set; }
+        public double LeftSndDer { get; set; }
+        public double RightSndDer { get; set; }
+        private SplineData? spline;
+        public SplineData? Spline {
+            get { return spline; }
+            set {
+                spline = value;
+                OnPropertyChanged("Spline");
+            }
+        }
+        public void Save(string filename) {
+            double[] Ends = new double[] { LEnd, REnd };
+            RawData rawData =  new RawData(Ends, NodesNum, IsUnuform, FuncType);
+            rawData.Save(filename);
+        }
+        public void Load(string filename) {
+            RawData rawData = new RawData(filename);
+            try {
+                NodesNum = rawData.NumOfNodes;
+                LEnd = rawData.Ends[0];
+                REnd = rawData.Ends[1];
+                IsUnuform = rawData.IsUnuform;
+                FuncType = rawData.Func;
+            }
+            catch (Exception ex) {
+                string messageBoxText = ex.ToString();
+                string caption = "Ошибочка";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBoxResult result;
+                result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+            }
+            Raw = rawData;
+        }
+        public ViewData() {
+            NodesNum = 6;
+            LEnd = 0;
+            REnd = 5;
+            IsUnuform = true;
+            FuncType = FRawEnum.Linear;
+
+            SplineNodesNum = 11;
+            LeftSndDer = 0;
+            RightSndDer = 0;
+            Raw = null;
+            Spline = null;
+        }
+
+        public void CreateRawData() {
+            double[] Ends = new double[] { LEnd, REnd };
+            Raw = new RawData(Ends, NodesNum, IsUnuform, FuncType);
+        }
+
+        public void ExiquteSpline() {
+            CreateRawData();
+            SplineData splineData = new SplineData(Raw, LeftSndDer, RightSndDer, SplineNodesNum);
+            splineData.Spline();
+            Spline = splineData;
+        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+    
+}
